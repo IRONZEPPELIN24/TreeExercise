f403cec80b821c3b1266a2938bf4ed853160c3dd
Tim Cotanch <tanch44@gmail.com>
2015-06-04 11:11:36 -0400

    Created basic framework for exercise

---
+++ TreeExercise/TreeExercise/Node.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreeExercise
+{
+    class Node
+    {
+        //Create the attributes 
+        private int data;
+        private Node rightChild;
+        private Node leftChild;
+
+        //Create the properties
+        public int Data
+        {
+            get { return data; }
+            set { data = value; }
+        }
+
+        public Node RightChild
+        {
+            get { return rightChild; }
+            set { rightChild = value; }
+        }
+
+        public Node LeftChild
+        {
+            get { return leftChild; }
+            set { leftChild = value; }
+        }
+
+        public Node(int data)
+        {
+            this.data = data;
+        }
+    }
+}
---
+++ TreeExercise/TreeExercise/Tree.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreeExercise
+{
+    class Tree
+    {
+        //Attributes
+        private Node root; 
+
+        //Methods needed for the Tree class
+        public void Create(int data)
+        {
+            if (root = null)
+            {
+                root = new Node(data);
+            }
+            else
+            {
+                CreateTree(data, root);
+            }
+        }
+        public void Print()
+        {
+            if (root == null)
+            {
+                throw new Exception("No Data");
+            }
+
+            else
+            {
+                PrintTree(root, 0);
+            }
+        }
+
+        private void CreateTree(int data, Node node)
+        {
+            
+        }
+
+        private void PrintTree(Node node, int level)
+        {
+
+        }
+    }
+}
